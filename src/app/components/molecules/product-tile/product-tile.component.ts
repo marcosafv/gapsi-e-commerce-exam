45e9481985456bcb80244ba0d1f2c2178d0f6d41
Marcos Flores <alejandro.visso@gmail.com>
2026-03-03 15:49:40 -0400

    feat: agregar efecto ripple a product-tile

--- src/app/components/molecules/product-tile/product-tile.component.ts
+++ src/app/components/molecules/product-tile/product-tile.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatRippleModule } from '@angular/material/core';
 import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product-tile',
   standalone: true,
-  imports: [CurrencyPipe, DragDropModule],
+  imports: [CurrencyPipe, DragDropModule, MatRippleModule],
   templateUrl: './product-tile.component.html',
   styleUrl: './product-tile.component.css',
 })
